3d7472a5d6768f28206e56d6fb5e99f95cde5dee
Martin Bravo <bravomartin@gmail.com>
2017-06-30 10:57:36 -0400

    stop trying static files

--- next/components/base/header.js
+++ next/components/base/header.js
@@ -9,7 +9,7 @@ export default ({viewer, title}) => (
   <header>
     <Head>
       <title>{title ? title : "Artefact"}</title>
-      <link rel="icon" type="image/png" href="/static/favicon.png" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
     </Head>
     <style jsx global>{baseStyles}</style>
     <Menu viewer={viewer} />
--- next/server.js
+++ next/server.js
@@ -12,11 +12,9 @@ const handle = app.getRequestHandler()
 app.prepare()
 .then(() => {
   const server = express()
-  server.use(express.static('public'))
   server.use(cookieParser())
   server.use(bodyParser.json());
   server.use(bodyParser.urlencoded({ extended: true }));
-  // server.use('/public/*', express.static('public'))
 
   // A simple route to set a httpOnly cookie with the
   // session id for the API. Otherwise we can't get the
@@ -26,13 +24,7 @@ app.prepare()
     res.cookie('artefact.sid', sessionId, { maxAge: 28908000000, httpOnly: true });
     return res.status(200).json({message: "Cookie created with session id: " + sessionId});
   })
-  // server.get('/public/*', (req,res) => {
-  //   console.log('in public')
-  //   const parsedUrl = parse(req.url, true)
-  //   const path = join(__dirname, '../', parsedUrl.pathname)
-  //   console.log(path)
-  //   return app.serveStatic(req, res, path)
-  // })
+
   server.get('*', (req, res) => {
     return handle(req, res)
   })
