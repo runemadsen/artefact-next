e7cde31fe27abcd907857d8853749cb0034fa1b4
Rune Madsen <rune@runemadsen.com>
2017-06-30 12:04:14 -0400

    changed DB migrations

--- migrations/sql/001.do.init.sql
+++ migrations/sql/001.do.init.sql
@@ -2,9 +2,9 @@ CREATE TABLE users (
   id bigserial primary key,
   username text NOT NULL UNIQUE,
   email text NOT NULL UNIQUE,
-  password_salt text NOT NULL,
-  password_hash text NOT NULL,
-  created_at timestamp with time zone NOT NULL default now()
+  passwordSalt text NOT NULL,
+  passwordHash text NOT NULL,
+  createdAt timestamp with time zone NOT NULL default now()
 );
 
 CREATE TABLE "session" (
@@ -17,12 +17,12 @@ ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFE
 
 CREATE TABLE people (
   id bigserial primary key,
-  user_id bigint REFERENCES users (id),
+  userId bigint REFERENCES users (id),
   type text,
   name text,
   company text,
-  address_1 text,
-  address_2 text,
+  address1 text,
+  address2 text,
   city text,
   state text,
   postal integer,
@@ -32,43 +32,43 @@ CREATE TABLE people (
 
 CREATE TABLE works (
   id bigserial primary key,
-  user_id bigserial REFERENCES users (id),
+  userId bigserial REFERENCES users (id) NOT NULL,
   title text,
   medium text,
   width numeric,
   height numeric,
   depth numeric,
-  dimension_unit text,
-  dimension_text text,
+  dimensionUnit text,
+  dimensionText text,
   editioned boolean DEFAULT false,
-  artist_id bigint REFERENCES people (id),
-  created_at timestamp with time zone
+  artistId bigint REFERENCES people (id),
+  createdAt timestamp with time zone
 );
 
 CREATE TABLE editions (
   id bigserial primary key,
-  edition_number integer NOT NULL,
-  edition_type text NOT NULL,
+  editionNumber integer NOT NULL,
+  editionType text NOT NULL,
   price numeric,
   currency text DEFAULT 'USD',
   status text,
-  work_id bigserial REFERENCES works (id),
-  collection_id bigint REFERENCES people (id),
-  location_id bigint REFERENCES people (id)
+  workId bigserial REFERENCES works (id),
+  collectionId bigint REFERENCES people (id),
+  locationId bigint REFERENCES people (id)
 );
 
-CREATE FUNCTION calculate_edition_number()
+CREATE FUNCTION calculateEditionNumber()
 RETURNS trigger AS '
 BEGIN
-  NEW.edition_number := 1 + (SELECT COUNT(*) FROM editions WHERE work_id = NEW.work_id AND edition_type = NEW.edition_type);
+  NEW.editionNumber := 1 + (SELECT COUNT(*) FROM editions WHERE workId = NEW.workId AND editionType = NEW.editionType);
   RETURN NEW;
 END' LANGUAGE 'plpgsql';
 
-CREATE TRIGGER edition_number BEFORE INSERT ON editions FOR EACH ROW EXECUTE PROCEDURE calculate_edition_number();
+CREATE TRIGGER editionNumber BEFORE INSERT ON editions FOR EACH ROW EXECUTE PROCEDURE calculateEditionNumber();
 
 
 CREATE TABLE images (
   id bigserial primary key,
   url text,
-  work_id bigint REFERENCES works (id)
+  workId bigint REFERENCES works (id)
 );
