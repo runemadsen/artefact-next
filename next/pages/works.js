00e948054b6e74cf40406971846453ed995f1356
Martin Bravo <bravomartin@gmail.com>
2017-07-07 16:17:22 -0400

    add viewer to work query

--- next/pages/works.js
+++ next/pages/works.js
@@ -1,22 +1,21 @@
+import Header from "../components/base/header";
+import Main from "../components/base/main";
+import Container from "../components/base/container";
+import Menu from "../components/base/menu";
+import Button from "../components/fields/button";
+import { graphqlRequest } from "../utils/api";
+import { Router, Link } from "../routes";
 
-import Header from '../components/base/header';
-import Main from '../components/base/main';
-import Container from '../components/base/container';
-import Menu from '../components/base/menu';
-import Button from '../components/fields/button';
-import { graphqlRequest } from '../utils/api';
-import { Router, Link } from '../routes';
-
-const newWorkSubmit = async (e) => {
-  e.preventDefault()
+const newWorkSubmit = async e => {
+  e.preventDefault();
   const res = await graphqlRequest(`mutation {
     createWork {
       id
     }
-  }`)
+  }`);
   const json = await res.json();
-  Router.pushRoute('Work', {id: json.data.createWork.id});
-}
+  Router.pushRoute("Work", { id: json.data.createWork.id });
+};
 
 const Works = props => {
   console.log(props.viewer);
@@ -27,34 +26,34 @@ const Works = props => {
 
         <h1>Works</h1>
         <Container right>
-          <Link to="/newwork"><a><Button small label="New Work" /></a></Link>
+          <Button
+            type="button"
+            small
+            label="New Work"
+            onClick={newWorkSubmit}
+          />
         </Container>
         <Container>
 
-
-      <h1>Works</h1>
-      <Container right>
-        <Button type="button" small label="New Work" onClick={newWorkSubmit} />
-      </Container>
-      <Container>
-
-        <ul>
-          {props.viewer.works.edges.map(work =>
-            <li key={work.node.id}>
-              {work.node.title} &nbsp;&nbsp;
-              <Link route='Work' params={{id: work.node.id }}><a>Edit work</a></Link>
-            </li>
-          )}
-        </ul>
-      </Container>
-    </Main>
-  </div>)
-
-}
+          <ul>
+            {props.viewer.works.edges.map(work =>
+              <li key={work.node.id}>
+                {work.node.title} &nbsp;&nbsp;
+                <Link route="Work" params={{ id: work.node.id }}>
+                  <a>Edit work</a>
+                </Link>
+              </li>
+            )}
+          </ul>
+        </Container>
+      </Main>
+    </div>
+  );
+};
 
 Works.getInitialProps = async ({ req }) => {
-
-  const res = await graphqlRequest(`query {
+  const res = await graphqlRequest(
+    `query {
     viewer {
       id
       username
@@ -72,8 +71,10 @@ Works.getInitialProps = async ({ req }) => {
         }
       }
     }
-  }`, req)
-  const json = await res.json()
-  return json.data
-}
+  }`,
+    req
+  );
+  const json = await res.json();
+  return json.data;
+};
 export default Works;
