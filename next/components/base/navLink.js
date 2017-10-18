09b6fe1a8d5609ee74042547c85ce827cf4b8692
Martin Bravo <bravomartin@gmail.com>
2017-10-18 10:09:57 -0300

    wrap Link content inside a

--- next/components/base/navLink.js
+++ next/components/base/navLink.js
@@ -1,22 +1,24 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'react-router'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router';
 
-import classnames from 'classnames'
+import classnames from 'classnames';
 
 class NavLink extends Component {
-  render(){
-    let classes = classnames("nav-link", {
-      "link-active" :  RegExp(this.props.to).test(this.props.currentPath)
-    } )
+  render() {
+    let classes = classnames('nav-link', {
+      'link-active': RegExp(this.props.to).test(this.props.currentPath)
+    });
     return (
-      <Link className={classes} to={this.props.to}>{this.props.children}</Link>
-      )
+      <Link className={classes} to={this.props.to}>
+        <a>{this.props.children}</a>
+      </Link>
+    );
   }
 }
 
 NavLink.propTypes = {
   to: PropTypes.string.isRequired
-}
+};
 
-export default NavLink
+export default NavLink;
